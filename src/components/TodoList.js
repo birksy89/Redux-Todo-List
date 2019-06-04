8017492c8250b4a58d3acc22e03dec83bdc7626b
Andrew <andy.birks@gmail.com>
2019-06-04 16:08:19 +0100

    Formatting

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,49 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
 // Import the possible actions
 import { addTodo } from "../actions/actionCreators";
 
 class TodoList extends Component {
+  renderTodos = (data = []) => {
+    return data.map(item => <li>{item}</li>);
+  };
 
-    renderTodos = (data = []) => {
-        return data.map(item =>  <li>{item}</li>)
-    }
+  handleButton = () => {
+    this.props.addTodo("Dummy Item...");
+  };
 
-    handleButton = () => {
-        this.props.addTodo("Dummy Item...")
-        
-    }
-    
-    render() {
-        const {todoData} = this.props;
-        return (
-            <div>
-                <button onClick={() => this.handleButton()}>Add Dummy Item</button>
-                <ul>
-                    {this.renderTodos(todoData)}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    const { todoData } = this.props;
+    return (
+      <div>
+        <button onClick={() => this.handleButton()}>Add Dummy Item</button>
+        <ul>{this.renderTodos(todoData)}</ul>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    console.log(state);
-    
-    return {
-      todoData: state.todo
-    };
+  console.log(state);
+
+  return {
+    todoData: state.todo
   };
-  
-  const mapDispatchToProps = {
-    addTodo
-    // increment,
-    // decrement,
-    // reset
-  };
-  
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(TodoList);
-  
+};
+
+const mapDispatchToProps = {
+  addTodo
+  // increment,
+  // decrement,
+  // reset
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
